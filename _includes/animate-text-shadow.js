a78fc627de10289d6906857c356e3e450e61624b
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-07-28 15:12:52 -0500

    Reset initial shadow onmouseleave

--- _includes/animate-text-shadow.js
+++ _includes/animate-text-shadow.js
@@ -14,3 +14,8 @@ h1.addEventListener("mousemove", function(e) {
     root.style.setProperty("--text-shadow-y", calculateShadow(e.clientY - h1.offsetTop, h1.clientHeight));
   });
 });
+
+h1.addEventListener("mouseleave", function(e) {
+  root.style.removeProperty("--text-shadow-x");
+  root.style.removeProperty("--text-shadow-y");
+});
