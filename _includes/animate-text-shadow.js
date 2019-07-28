62a3c431e3299f71878258bec503a8f04bb2fde3
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-07-28 14:42:56 -0500

    Dedicated pages for talks

--- _includes/animate-text-shadow.js
+++ _includes/animate-text-shadow.js
@@ -1,20 +1,16 @@
-var maxShadow = 5;
 var root = document.documentElement;
 var h1 = document.querySelector("h1");
 var lastRequestId;
 
-log = (x) => { console.log(x); return x }
-
 function calculateShadow(rawCursor, length) {
   var ratio = Math.max(0, rawCursor) / length;
-  return maxShadow - (ratio * maxShadow * 2);
+  return 1 - (ratio * 2);
 }
 
 h1.addEventListener("mousemove", function(e) {
-  if (lastRequestId) cancelAnimationFrame(lastRequestId);
+  cancelAnimationFrame(lastRequestId);
   lastRequestId = requestAnimationFrame(function() {
     root.style.setProperty("--text-shadow-x", calculateShadow(e.clientX - h1.offsetLeft, h1.clientWidth));
     root.style.setProperty("--text-shadow-y", calculateShadow(e.clientY - h1.offsetTop, h1.clientHeight));
-    cancelAnimationFrame(lastRequestId);
   });
 });
