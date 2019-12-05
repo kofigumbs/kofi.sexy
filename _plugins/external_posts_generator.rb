d5cbc1e354769a07a776f019eca6012ace85a474
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-12-05 12:33:37 -0500

    Re-add external posts to feed

---
+++ _plugins/external_posts_generator.rb
@@ -0,0 +1,33 @@
+require "jekyll"
+
+module Jekyll
+  class ExternalPostsGenerator < Generator
+    safe true
+    priority :highest
+
+    def generate(site)
+      posts = site.collections["posts"]
+      site.config["external_posts"].each do |yaml|
+        doc = ExternalPost.new yaml["url"], site: site, collection: posts
+        doc.data["permalink"]  = yaml["url"]
+        doc.data["title"]      = yaml["title"]
+        doc.data["date"]       = yaml["date"]
+        doc.data["collection"] = "posts"
+        doc.data["categories"] = ["blog"]
+        doc.data["draft"]      = false
+        posts.docs << doc
+      end
+    end
+  end
+
+  class ExternalPost < Document
+    def initialize(url, options)
+      super("", options)
+      @url = url
+      @content = "View this post at <#{url}>"
+    end
+
+    def write(*_)
+    end
+  end
+end
