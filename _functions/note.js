7ca06269d816e80670b9d8f8000ea30c8ba303e6
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-03-14 11:28:02 -0400

    Contenteditable notes

--- _functions/note.js
+++ _functions/note.js
@@ -1,31 +1,31 @@
-function decode(q) {
+function q(event) {
   try {
-    return decodeURIComponent(q || "");
+    return decodeURIComponent(event.queryStringParameters.q || "").replace(/[<>]/g, "");
   } catch {
     return "";
   }
 }
 
-function html(body) {
-  return `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-        <link rel="stylesheet" href="/styles.css">
-      </head>
-      <body><main>${body}</main></body>
-    </html>`;
-}
-
-exports.handler = (event, _, callback) => {
-  const q = decode(event.queryStringParameters.q).replace(/[<>]/g, "");
-  const body = q ? html(`<p style="white-space: pre-line">${q}</p>`) : html(`
-    <textarea></textarea>
-    <script type="text/javascript">
-      document.querySelector("textarea").addEventListener("input", function(event) {
-        window.history.replaceState(null, null, "?q=" + encodeURIComponent(event.target.value));
-      });
-    </script>`);
-  callback(null, { statusCode: 200, body: body, headers: {} });
+exports.handler = function(event, _, callback) {
+  callback(null, {
+    statusCode: 200,
+    headers: {},
+    body: `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+          <link rel="stylesheet" href="/styles.css">
+        </head>
+        <body>
+          <header contenteditable style="white-space: pre-line">${q(event)}</header>
+          <br>
+          <script type="text/javascript">
+            document.querySelector("header").addEventListener("input", function(event) {
+              window.history.replaceState(null, null, "?q=" + encodeURIComponent(event.target.innerText));
+            });
+          </script>
+        </body>
+      </html>`,
+  });
 };
