200ba4b31f69eeb196ddf4651e86c879f51ec350
Kofi Gumbs <h.kofigumbs@gmail.com>
2023-07-06 23:31:07 -0400

    Simplify site build

--- Gemfile
+++ Gemfile
@@ -2,9 +2,14 @@ source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-gem "github-pages"
-gem "mp3info"
+gem "jekyll"
 gem "tare", github: "kofigumbs/tare", ref: "bfd61db"
+
+group :jekyll_plugins do
+  gem "jekyll-commonmark"
+  gem "jekyll-feed"
+  gem "jekyll-seo-tag"
+end
 
 group :development do
   gem "webrick", "~> 1.8"
--- _plugins/generator.rb
+++ _plugins/generator.rb
@@ -1,5 +1,4 @@
 require "jekyll"
-require "mp3info"
 
 class Generator < Jekyll::Generator
   priority :highest
@@ -7,15 +6,6 @@ class Generator < Jekyll::Generator
   def generate(site)
     # Load profile.jpg as base64 for use in the favicon
     site.data['profile_base64'] = %x(cat _www/images/profile.jpg | base64)
-
-    # Set the correct MP3 tag metadata for each track
-    Dir.glob("_www/music/*.mp3").each do |file|
-      Mp3Info.open(file, parse_tags: true, parse_mp3: false) do |mp3|
-        mp3.tag.title  = file.match(/-(.+)\.mp3/)[1]
-        mp3.tag.album  = mp3.tag.title
-        mp3.tag.artist = "Kofi Gumbs"
-      end
-    end
 
     # Mix external posts (from _config.yml) into feed
     posts = site.collections["posts"]
